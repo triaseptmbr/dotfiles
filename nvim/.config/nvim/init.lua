5bd0570e9fe91b537a7ca0eb428865fe09779169
Tri Asep Tumbara <triaseptmbr@gmail.com>
2025-10-29 14:37:43 +0700

    Modify Neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -2,15 +2,16 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin')
 
-Plug('craftzdog/solarized-osaka.nvim')
+Plug('olimorris/onedarkpro.nvim')
 Plug('nvim-lua/plenary.nvim')
 Plug('nvim-telescope/telescope.nvim', { ['branch'] = '0.1.x' })
 Plug('nvim-lualine/lualine.nvim')
 Plug('nvim-tree/nvim-web-devicons')
+Plug('nvim-treesitter/nvim-treesitter', { ['branch'] = 'master', ['do'] = ':TSUpdate'})
 
 vim.call('plug#end')
 
-vim.cmd('colorscheme solarized-osaka')
+vim.cmd('colorscheme onedark')
 vim.g.mapleader = ' '
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -18,6 +19,9 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.autoindent = true
 vim.opt.expandtab = true
+vim.opt.wrap = true
+vim.opt.linebreak = true
+vim.opt.breakindent = true
 
 local function set_tab_width(width)
     vim.opt_local.tabstop = width
@@ -35,6 +39,23 @@ local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+require('telescope').setup({
+    defaults = {
+        layout_config = {
+            horizontal = {
+                width = 180
+            },
+            preview_width = 0.3
+        },
+    },
+})
 
 require('lualine').setup()
+
+require'nvim-treesitter.configs'.setup {
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+}
