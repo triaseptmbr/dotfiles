d291979608ae717db1136dd15e4876158ec0eec7
Tri Asep Tumbara <triaseptmbr@gmail.com>
2026-07-01 15:52:13 +0700

    Change the package manager to LazyLua

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,30 +1,13 @@
-local vim = vim
-local Plug = vim.fn['plug#']
+require("config.lazy")
 
-vim.call('plug#begin')
-
-Plug('Mofiqul/dracula.nvim')
-Plug('nvim-lua/plenary.nvim')
-Plug('nvim-lualine/lualine.nvim')
-Plug('nvim-telescope/telescope.nvim', { ['branch'] = '0.1.x' })
-Plug('nvim-tree/nvim-tree.lua')
-Plug('nvim-tree/nvim-web-devicons')
-Plug('nvim-treesitter/nvim-treesitter', { ['branch'] = 'master', ['do'] = ':TSUpdate'})
-Plug('tpope/vim-surround')
-
-vim.call('plug#end')
-
-vim.cmd('colorscheme dracula')
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-vim.g.mapleader = ' '
+vim.cmd.colorscheme('onedark')
 vim.opt.autoindent = true
 vim.opt.breakindent = true
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.expandtab = true
 vim.opt.linebreak = true
 vim.opt.number = true
-vim.opt.relativenumber = true
+vim.opt.relativenumber = false
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.wrap = true
@@ -35,31 +18,15 @@ local function set_tab_width(width)
 end
 
 vim.api.nvim_create_autocmd('FileType', {
-    pattern = { 'javascript', 'typescript', 'javascriptreact', 'typescriptreact', 'html', 'css' },
+    pattern = { 'javascript', 'html', 'css' },
     callback = function()
         set_tab_width(2)
     end,
 })
 
-vim.api.nvim_create_autocmd({ 'FocusGained', 'BufEnter', 'CursorHold' }, {
-    command = 'checktime',
+vim.o.autoread = true
+    vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
+        command = "checktime",
 })
 
-require'nvim-treesitter.configs'.setup {
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
-}
-
-require("nvim-tree").setup()
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-
-vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', { desc = 'Toggle NvimTree' })
-vim.keymap.set('n', '<leader>n', ':NvimTreeFocus<CR>', { desc = 'Focus NvimTree' })
-vim.keymap.set('n', '<leader>f', ':NvimTreeFindFile<CR>', { desc = 'Find current file in NvimTree' })
-vim.keymap.set('n', '<leader>c', ':NvimTreeCollapse<CR>', { desc = 'Collapse NvimTree' })
+require('lualine').setup()
---
+++ nvim/.config/nvim/lua/plugins/plugins.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "olimorris/onedarkpro.nvim",
+        priority = 1000
+    },
+    {
+        'sheerun/vim-polyglot'
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' }
+    }
+}
